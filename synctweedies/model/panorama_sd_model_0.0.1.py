7c69e67edaaa230a6c0952d6ad3b05fe174cccee
Byeonghyun Pak <byeonghyun.pak@gmail.com>
2024-12-20 07:03:54 +0900

    (v0.0.1) initial commit of discrete_warping

--- synctweedies/model/panorama_sd_model_0.0.1.py
+++ synctweedies/model/panorama_sd_model_0.0.1.py
@@ -107,3 +107,54 @@ class PanoramaSDModel_0_0_1(PanoramaSDModel):
         # 3) Add the pixel value to it
         up_noise = upscaled_means / up_factor + mean_removed_rand
         return up_noise # sqrt(N_k)* W(A_k): sub-pixel noise scaled with sqrt(N_k). So, ~ N(0, 1)
+
+    @torch.no_grad()
+    def discrete_warping(self, src_noise, hw=(512//8, 512//8)):
+        B, C, H, W = src_noise.shape
+        h, w = hw
+
+        # ERP pixel coordinate (normalized to [-1, 1])
+        erp_grid = make_coord((H, W), flatten=False).to(self.device) # (H, W, 2)
+        
+        # Pers. pixel index (range: [1, hw])
+        pers_idx = torch.arange(1, h*w+1, device=self.device, dtype=torch.float32).view(1, h, w) # (1, h, w)
+
+        # flatten ERP noise
+        erp_up_noise_flat = src_noise.reshape(B*C, -1)
+        # flatten count value
+        ones_flat = torch.ones_like(erp_up_noise_flat[:1])
+
+        pers_noises = []
+        erp2pers_idxs = []
+
+        for theta, phi in self.views:
+
+            # Warp ERP pixel grid into Pers. coordinate
+            erp2pers_grid, valid_mask = gridy2x_erp2pers(gridy=erp_grid,
+                HWy=(H, W), HWx=hw, THETA=theta, PHI=phi, FOVy=360, FOVx=90)
+            erp2pers_grid = erp2pers_grid.view(H, W, 2)
+            valid_mask = valid_mask.view(1, H, W)
+            
+            # Find which index of Pers. is mapped to ERP pixel
+            erp2pers_idx = F.grid_sample(
+                pers_idx.unsqueeze(0),
+                erp2pers_grid.unsqueeze(0).flip(-1),
+                mode="nearest", align_corners=False)[0] # (1, H, W)
+            erp2pers_idx *= valid_mask # non-mapped ERP pixel has 0 index
+            erp2pers_idx = erp2pers_idx.to(torch.int64) # index's dtype must be dtype
+
+            # Get warped Pers. pixel noise
+            ind_flat = erp2pers_idx.view(1, -1)
+            fin_v_val = torch.zeros(B*C, h*w+1, device=self.device) \
+                .scatter_add_(1, index=ind_flat.repeat(B*C, 1), src=erp_up_noise_flat)[..., 1:]
+            fin_v_num = torch.zeros(1, h*w+1, device=self.device) \
+                .scatter_add_(1, index=ind_flat, src=ones_flat)[..., 1:]
+            assert fin_v_num.min() != 0, ValueError(f"{theta},{phi} has 0 fin_v_num.")
+
+            # Normalize fin_v_val to scale Variance of Pers. pixel noise
+            final_values = fin_v_val / torch.sqrt(fin_v_num)
+
+            pers_noises.append(final_values.reshape(B, C, h, w).float())
+            erp2pers_idxs.append(erp2pers_idx.reshape((H, W)))
+
+        return pers_noises, erp2pers_idxs
